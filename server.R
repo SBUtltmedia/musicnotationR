0ca98efc4de58c81b0c6e904b7e8704e5248798b
Ken Fehling <ken@androidideas.org>
2016-03-17 17:52:59 -0400

    Got it working!

--- server.R
+++ server.R
@@ -9,7 +9,37 @@ library(shiny)
 source('R/musicnot.R')
 
 shinyServer(function(input, output) {
-  flies <- read.csv("data-raw/flies.csv", stringsAsFactors=F)
-  output$distPlot <- renderPlot(musicnot(flies, gridcolor=T, gridlinesize = 0.5, labels="name", colors=c("firebrick1", "dodgerblue", "seagreen", "darkgoldenrod1"), behav="yes"))
-
+  newdf <- read.csv("data-raw/ken.csv", stringsAsFactors=F)
+  df1 <- reactive({
+    mintime <- input$timeper[1]
+    maxtime <- input$timeper[2]
+    df1 <- newdf %>% filter(Time >= mintime & Time <= maxtime)
+    ranks <- input$checkGroup
+    if (input$graphtype == 1) {
+      if (input$winloser == 1) {
+        df1 <- df1[((df1$winner %in% ranks) == T),]
+      }
+      else if (input$winloser == 2) {
+        df1 <- df1[((df1$winner %in% ranks) == T | (df1$loser %in% ranks) == T),]
+      }
+      else {
+        df1 <- df1[((df1$loser %in% ranks) == T),]
+      }
+    }
+    else {
+      if (input$winloser == 1) {
+        df1 <- df1[(df1$winner %in% ranks ==T) & (df1$loser %in% ranks ==T),]    
+        df1 <- df1[((df1$winner %in% ranks) == T),]
+      }
+      else if (input$winloser == 2) {
+        df1 <- df1[(df1$winner %in% ranks ==T) & (df1$loser %in% ranks ==T),]    
+        df1 <- df1[((df1$winner %in% ranks) == T | (df1$loser %in% ranks) == T),]
+      }
+      else {
+        df1 <- df1[(df1$winner %in% ranks ==T) & (df1$loser %in% ranks ==T),]    
+      }
+      df1 <- df1[((df1$loser %in% ranks) == T),]
+    }    
+  })
+  output$distPlot <- renderPlot(musicnot(df1(), gridcolor=T, gridlinesize = 0.5, labels="name", colors=c("firebrick1", "dodgerblue", "seagreen", "darkgoldenrod1"), behav="yes"))
 })
